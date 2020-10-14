ffd4199412b604fb3ba02b842ac2f85ee758f473
akorelyakov <al.korelyakov@gmail.com>
2020-10-14 17:33:47 +0300

    implement HW2.1 HW2.2

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -24,7 +24,6 @@ public class InMemoryMealRepository implements MealRepository {
             repository.put(meal.getId(), meal);
             return meal;
         }
-        // handle case: update, but not present in storage
         return repository.computeIfPresent(meal.getId(), (id, oldMeal) -> meal);
     }
 
