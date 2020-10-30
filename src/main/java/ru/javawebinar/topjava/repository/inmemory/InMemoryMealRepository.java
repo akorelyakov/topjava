b134cc246fdb40cdbbdd20a64d85703d628a0477
akorelyakov <al.korelyakov@gmail.com>
2020-10-30 14:25:45 +0300

    implement meal layers

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -5,41 +5,58 @@ import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.util.MealsUtil;
 
 import java.util.Collection;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 public class InMemoryMealRepository implements MealRepository {
-    private final Map<Integer, Meal> repository = new ConcurrentHashMap<>();
+    private Map<Integer, Meal> meals = new ConcurrentHashMap<>();
+    private final Map<Integer, Map<Integer, Meal>> repository = new ConcurrentHashMap<>();
     private final AtomicInteger counter = new AtomicInteger(0);
 
-    {
-        MealsUtil.meals.forEach(this::save);
+    public InMemoryMealRepository() {
+        for (Meal meal : MealsUtil.meals) {
+            save(MealsUtil.USER_ID, meal);
+        }
     }
 
     @Override
-    public Meal save(Meal meal) {
+    public Meal save(int userId, Meal meal) {
+        meals = repository.get(userId);
         if (meal.isNew()) {
             meal.setId(counter.incrementAndGet());
-            repository.put(meal.getId(), meal);
+            meals.put(meal.getId(), meal);
+            repository.put(userId, meals);
             return meal;
         }
-        return repository.computeIfPresent(meal.getId(), (id, oldMeal) -> meal);
+        Meal newMeal = meals.computeIfPresent(meal.getId(), (id, oldMeal) -> meal);
+        repository.put(userId, meals);
+        return newMeal;
     }
 
     @Override
-    public boolean delete(int id) {
-        return repository.remove(id) != null;
+    public boolean delete(int userId, int id) {
+        meals = repository.get(userId);
+        boolean isDeleted = meals.remove(id) != null;
+        repository.put(userId, meals);
+        return isDeleted;
     }
 
     @Override
-    public Meal get(int id) {
-        return repository.get(id);
+    public Meal get(int userId, int id) {
+        meals = repository.get(userId);
+        return meals.get(id);
     }
 
     @Override
-    public Collection<Meal> getAll() {
-        return repository.values();
+    public List<Meal> getAll(int userId) {
+        meals = repository.get(userId);
+        return meals.values()
+                .stream()
+                .sorted(Comparator.comparing(Meal::getDate).reversed())
+                .collect(Collectors.toList());
     }
 }
-
--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -1,8 +1,42 @@
 package ru.javawebinar.topjava.web.meal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
 
+import java.util.List;
+
+import static ru.javawebinar.topjava.util.ValidationUtil.*;
+
 public class MealRestController {
+    protected final Logger log = LoggerFactory.getLogger(getClass());
     private MealService service;
 
+    public List<Meal> getAll(int userId) {
+        log.info("getAll");
+        return service.getAll(userId);
+    }
+
+    public Meal get(int userId, int id) {
+        log.info("get {}", id);
+        return service.get(userId, id);
+    }
+
+    public Meal create(int userId, Meal meal) {
+        log.info("create {}", meal);
+        checkNew(meal);
+        return service.create(userId, meal);
+    }
+
+    public void delete(int userId, int id) {
+        log.info("delete {}", id);
+        service.delete(userId, id);
+    }
+
+    public void update(int userId, Meal meal, int id) {
+        log.info("update {} with id={}", meal, id);
+        assureIdConsistent(meal, id);
+        service.update(userId, meal);
+    }
 }
