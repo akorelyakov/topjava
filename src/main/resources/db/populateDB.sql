6c5d40ef710c4fce62899a2098f2feb21349234f
akorelyakov <al.korelyakov@gmail.com>
2020-11-18 15:40:58 +0300

    implement JdbcMealRepository

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -9,3 +9,11 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_roles (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (user_id, datetime, description, calories)
+VALUES (100000, '2020-11-18 14:53:32.000000', 'user meal 1000', 1000),
+       (100000, '2020-11-18 15:57:32.000000', 'user meal 1100', 1100),
+       (100000, '2020-11-17 14:52:32.000000', 'user meal 800', 800),
+       (100001, '2020-11-18 13:53:32.000000', 'admin meal 1000', 1000),
+       (100001, '2020-11-18 15:53:32.000000', 'admin meal 1200', 1200),
+       (100001, '2020-11-17 14:54:32.000000', 'admin meal 800', 800);
