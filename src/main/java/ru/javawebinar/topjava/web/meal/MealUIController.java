864b5652c89e372c5b954da2592e3e929f99fd0c
akorelyakov <al.korelyakov@gmail.com>
2021-02-11 17:33:43 +0300

    add HW8-02

--- src/main/java/ru/javawebinar/topjava/web/meal/MealUIController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealUIController.java
@@ -1,5 +1,41 @@
 package ru.javawebinar.topjava.web.meal;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.lang.Nullable;
+import org.springframework.web.bind.annotation.*;
+import ru.javawebinar.topjava.to.MealTo;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+@RestController
+@RequestMapping("/profile/meals")
 public class MealUIController extends AbstractMealController {
 
+    @Override
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<MealTo> getAll() {
+        return super.getAll();
+    }
+
+    @Override
+    @DeleteMapping("/{id}")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable int id) {
+        super.delete(id);
+    }
+
+
+
+    @Override
+    @GetMapping(value = "/filter")
+    public List<MealTo> getBetween(
+            @RequestParam @Nullable LocalDate startDate,
+            @RequestParam @Nullable LocalTime startTime,
+            @RequestParam @Nullable LocalDate endDate,
+            @RequestParam @Nullable LocalTime endTime) {
+        return super.getBetween(startDate, startTime, endDate, endTime);
+    }
 }
